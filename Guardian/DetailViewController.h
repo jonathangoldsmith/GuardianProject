9fd7596f9a8d5560a1e73c5e24f98a274eccfe9e
Jonathan Goldsmith <jonathan.goldsmith@macys.com>
2015-01-16 21:59:17 -0800

    Initial commit from local git

---
+++ Guardian/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Guardian
+//
+//  Created by Jonathan Goldsmith on 1/16/15.
+//  Copyright (c) 2015 JonathanGoldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Guardian/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Guardian
+//
+//  Created by Jonathan Goldsmith on 1/16/15.
+//  Copyright (c) 2015 JonathanGoldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
